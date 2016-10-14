bf54ede7c95efb41294e9f8461e6e18aeea5c45d
Yehor Lvivski <lvivski@gmail.com>
2016-10-14 16:10:40 +0300

    work better with repeated keys

--- expeditor.js
+++ expeditor.js
@@ -2,45 +2,58 @@ var toRegex = require('path-to-regexp')
 var cache = {}
 
 module.exports = function expeditor (routes) {
-  return function (uri, params) {
-    if (!uri) throw new TypeError('expeditor requires URI')
+	return function (uri, params) {
+		if (!uri) throw new TypeError('expeditor requires URI')
 		params || (params == {})
 
-    for (var pattern in routes) {
-      var match = matchURI(pattern, uri, params)
-      if (match) {
+		for (var pattern in routes) {
+			var match = matchPattern(pattern, uri, params)
+			if (match) {
 				var handler = routes[pattern]
-        if (typeof handler !== 'function') return handler
-        else return handler(params)
-      }
-    }
+				if (typeof handler !== 'function') return handler
+				else return handler(params)
+			}
+		}
 
-    return null
-  }
+		return null
+	}
 }
 
 exports.compile = function (pattern) {
 	return toRegex.compile(pattern)
 }
 
-function matchURI(pattern, uri, params) {
-  var regex = cache[pattern]
-	if (!regex) {
-			var keys = []
+function getMatcher(pattern) {
+	if (!cache[pattern]) {
+		var keys = [],
 			regex = toRegex(pattern, keys)
-			cache[pattern] = regex
-	}
-  var match = regex.exec(uri)
 
-  if (!match) return false
-  else if (!params) return true
-
-  for (var i = 1; i < match.length; ++i) {
-    var key = keys[i - 1]
-    var val = typeof match[i] === 'string' ? decodeURIComponent(match[i]) : match[i]
-    if (key) {
-			params[key.name] = val
+		cache[pattern] = {
+			regex: regex,
+			keys: keys
 		}
-  }
-  return true
+	}
+	return cache[pattern]
+}
+
+function matchPattern(pattern, uri, params) {
+	var matcher = getMatcher(pattern),
+		match = matcher.regex.exec(uri)
+
+	if (!match) return false
+	else if (!params) return true
+
+	var keys = matcher.keys
+	for (var i = 1; i < match.length; ++i) {
+		var key = keys[i - 1],
+			val = decodeURIComponent(match[i])
+
+		if (key) {
+			params[key.name] = val
+			if (key.repeat) {
+				params[key.name] = params[key.name].split(key.delimiter)
+			}
+		}
+	}
+	return true
 }
